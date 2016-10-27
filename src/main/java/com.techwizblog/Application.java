8c1dd2011632be5b4e6dab58f2c92774d0cbcf6e
SIB <techwizblog>
2016-10-28 09:34:18 +1100

    Init

---
+++ src/main/java/com.techwizblog/Application.java
@@ -0,0 +1,16 @@
+package com.techwizblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by SIB on 8/12/2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
